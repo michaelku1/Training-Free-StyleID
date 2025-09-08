77d3ae15c426f1a96407cbe953a568ec020bc298
michael <mku666@gmail.com>
2025-09-08 02:52:23 +0000

    fix scheduler

--- job_scheduler.sh
+++ job_scheduler.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 while true; do
-    FREE=$(gpustat --json | jq '.gpus[0].memory.free')
-    if [ "$FREE" -gt 1000 ]; then
+    USED=$(gpustat --json | jq '.gpus[1]["memory.used"]')
+    if [ "$USED" -lt 10000 ]; then
         echo "Running job!"
-        ./your_script.sh
+        CUDA_VISIBLE_DEVICES=2 python -m riffusion.server --host 127.0.0.1 --port 8080
         break
+    else
+        echo "GPU 1 is currently full (used: $USED MB)"
     fi
+
     sleep 5
 done
